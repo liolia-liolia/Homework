bd4d6f1bdc8fe739e2dfe808d3fa70b2af62b203
Olga <komiagina.olya@yandex.ru>
2022-12-24 22:53:53 +0300

    Добавила задачи 1

---
+++ C#/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+/*Console.WriteLine("Задача 1: напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.");
+Console.Write("Введите трехзначное число: ");
+int number = new Random().Next(100,1000);
+int secondNumber = number / 10 % 10;
+Console.WriteLine($"Вторая цифра в числе {number} -> {secondNumber}");*/
+
+/*Console.WriteLine("Задача 2: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.");
+Console.Write("Введите число:");
+int num = int.Parse(Console.ReadLine()!);
+if(num < 100)
+{
+    Console.WriteLine($"Третьей цифры нет!");
+}
+if(num >= 100 && num <= 999){
+    int thirdNum = num % 10;
+    Console.WriteLine($"Третья цифра в числе {num} -> {thirdNum}");
+}
+if(num >= 1000 && num <= 9999){
+    int thirdNum = (num % 100) / 10;
+    Console.WriteLine($"Третья цифра в числе {num} -> {thirdNum}");
+}
+if(num >= 10000 && num <= 99999){
+    int thirdNum = (num % 1000) / 100;
+    Console.WriteLine($"Третья цифра в числе {num} -> {thirdNum}");
+}
+if(num > 100000){
+    Console.WriteLine($"Неверное число");
+}*/
+
+/*Console.WriteLine("Задача 3: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.");
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+if((num > 7) || (num < 1))
+{
+    Console.WriteLine("Невыходной");
+    return;
+}
+if((num > 5))
+{
+    Console.WriteLine("Да");
+}
+else
+{
+   Console.WriteLine("Нет"); 
+}*/
