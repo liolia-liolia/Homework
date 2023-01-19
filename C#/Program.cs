ae2af5cef641291cc4e3b69fc6fb3d319fb3d784
Olga <komiagina.olya@yandex.ru>
2023-01-19 18:44:49 +0300

    Добавила все файлы

--- C#/Program.cs
+++ C#/Program.cs
@@ -88,3 +88,80 @@ else
 {
     Console.WriteLine($"{number} - не является пятизначным");
 }*/
+
+//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+/*int numberA = ReadInt("Введите число A: ");
+int numberB = ReadInt("Введите число B: ");
+ToDegree(numberA, numberB);
+
+
+//Функция возведения в степень
+void ToDegree(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+       result = result * a; 
+    }
+    Console.WriteLine(result);
+}
+//Функция ввода
+ int ReadInt(string message)
+ {
+    Console.WriteLine(message);
+    return
+    Convert.ToInt32(Console.ReadLine()!);
+ }*/
+
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+/*int number = ReadInt("Введите число: ");
+int len = NumberLen(number);
+SumNumbers(number, len);
+
+//Функция ввода
+ int ReadInt(string message)
+ {
+    Console.WriteLine(message);
+    return
+    Convert.ToInt32(Console.ReadLine()!);
+ }
+
+ //Функция подсчета цифр в числе
+ int NumberLen(int a)
+ {
+    int index = 0;
+    while (a > 0)
+    {
+        a /= 10;
+        index++;
+    }
+    return index;
+ }
+
+ //Функция вывода суммы цифр в числе
+ void SumNumbers(int n, int len)
+ {
+    int sum = 0;
+    for (int i = 1; i<= len; i++)
+    {
+        sum += n % 10;
+        n /= 10;
+    }
+    */
+
+//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
+/*int lenArray = ReadInt("Введите длину массива:");
+int[] randomArray = new int[lenArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(1,9);
+    Console.Write(randomArray[i] + " ");
+}
+
+//Функция ввода
+ int ReadInt(string message)
+ {
+    Console.WriteLine(message);
+    return
+    Convert.ToInt32(Console.ReadLine()!);
+ }*/
