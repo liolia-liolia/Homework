81db68051317036c63c5fd53317e1ff7e0da509e
Olga <komiagina.olya@yandex.ru>
2023-01-23 16:42:20 +0300

    Задачи 5 семинара

--- C#/Program.cs
+++ C#/Program.cs
@@ -165,3 +165,82 @@ for (int i = 0; i < randomArray.Length; i++)
     return
     Convert.ToInt32(Console.ReadLine()!);
  }*/
+
+ //Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+ /*int[] startArray = GetArray(6, 100, 999);
+ Console.WriteLine(String.Join(" ", startArray));
+ Console.WriteLine($"Количество четных элементов в массиве = {PositivCount(startArray)}");
+ 
+ int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int PositivCount(int[] array)
+{
+int count = 0;
+    foreach (int el in startArray)
+    {
+        if (el % 2 == 0) count++;
+    }
+    return count;
+}*/
+
+//Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных индексах.
+/*int[] startArray = GetArray(6, 0, 999);
+ Console.WriteLine(String.Join(" ", startArray));
+ Console.WriteLine($"Сумма элементов с нечетными индексами = {GetSum(startArray)}");
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int GetSum (int[] array)
+{ 
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 == 1) sum += array[i];
+    }
+    return sum;
+}*/
+
+//Задача 37: Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+/*int[] startArray = GetArray(5, 0, 5);
+ Console.WriteLine(String.Join(" ", startArray));
+ 
+ Console.WriteLine(String.Join(" ", ResultArray(startArray)));
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int[] ResultArray (int[] array)
+{
+    int size = array.Length / 2;
+    if (array.Length % 2 == 1) size++;
+    int[] result = new int[size];
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        result[i] = array[i] * array[array.Length - 1 - i];
+    }
+    if (array.Length % 2 == 1) result[size - 1] = array[array.Length / 2];
+    return result;
+}*/
